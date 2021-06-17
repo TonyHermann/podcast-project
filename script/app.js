97928bba217e245bd3e01eaa55a10d3f66978066
TonyHermann <alejoh.developer@gmail.com>
2021-06-16 23:58:49 -0300

    Starting the project

---
+++ script/app.js
@@ -0,0 +1,8 @@
+// let boton = document.getElementById("boton");
+
+// let wh = () => {
+//     console.log("Width "+document.documentElement.clientWidth);
+//     console.log("Height "+document.documentElement.clientHeight);
+// }
+
+// boton.addEventListener("click", wh);
